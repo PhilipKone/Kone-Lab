6b8482bcd7f417ffba4c642ddca388ace47cffc9
Kone <philipkone45@.com>
2026-03-13 00:40:06 +0000

    Updates

--- sw.js
+++ sw.js
@@ -1,15 +1,26 @@
-const CACHE_NAME = 'kone-lab-cache-v1';
+const CACHE_NAME = 'kone-lab-cache-v2';
 const urlsToCache = [
   './',
   './index.html',
-  './logo192.png',
-  './logo512.png'
+  './logo-circle-blue.svg'
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
   );
 });
 
